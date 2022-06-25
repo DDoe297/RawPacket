fe9d19d8e75b7c27aaaff88bef72da95d80d5335
Danny Khorasanizadeh <dannykhorasanizadeh@ec.iut.ac.ir>
2022-06-25 20:42:05 +0430

    Update pkt_sender.py

--- pkt_sender.py
+++ pkt_sender.py
@@ -1,7 +1,8 @@
 import socket
+
 content = bytes.fromhex(input('What is your packet content?'))
 interface = input('Which interface do you want to use?')
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
 s.bind((interface, 0))
-s.send(content)
-print(f'Sent {len(content)}-byte packet on {interface}')
+length = s.send(content)
+print(f'Sent {length}-byte packet on {interface}')
